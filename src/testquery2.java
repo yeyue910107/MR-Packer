c80f9d644a5530755809325ea431eb44c291b7c8
yeyue <yeyue@ubuntu.(none)>
2013-11-05 21:02:53 +0800

    update

--- src/testquery2.java
+++ src/testquery2.java
@@ -1,3 +1,4 @@
+package test;
 import java.io.IOException;
 import java.util.*;
 import java.text.*;
@@ -12,84 +13,106 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
 import org.apache.hadoop.mapreduce.lib.partition.*;
 
 
-	public static class Map extends  Mapper<Object, Text, IntWritable, Text>{
+public class testquery2 extends Configured implements Tool {
+
+	public static class Map extends Mapper<Object, Text, IntWritable, Text> {
 
 		private String filename;
 		private int filetag = -1;
 		public void setup(Context context) throws IOException, InterruptedException {
-
-			int last_index = -1, start_index = -1;
+			int last_index = -1;
 			String path = ((FileSplit)context.getInputSplit()).getPath().toString();
 			last_index = path.lastIndexOf('/');
-			last_index = last_index - 1;
-			start_index = path.lastIndexOf('/', last_index);
-			filename = path.substring(start_index + 1, last_index + 1);
-			if (filename.compareTo("testquery3") == 0){
-				filetag = 1;
-			}
-			if (filename.compareTo("NATION") == 0){
+			filename = path.substring(last_index + 1);
+			if (filename.compareTo("LINEITEM") == 0) {
 				filetag = 2;
+			}
+			if (filename.compareTo("PART") == 0) {
+				filetag = 1;
 			}
 		}
 
-		public void map(Object key, Text value, Context context) throws IOException, InterruptedException{
-
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 			String line = value.toString();
 			String[] line_buf= line.split("\\|");
 			BitSet dispatch = new BitSet(32);
-			if (filetag == 1){
-
-				context.write(new IntWritable(Integer.parseInt(line_buf[8])), new Text(1+"||"+Integer.parseInt(line_buf[0])+line_buf[1]+Double.parseDouble(line_buf[2])+line_buf[3]+line_buf[4]+line_buf[5]+Double.parseDouble(line_buf[6])+Double.parseDouble(line_buf[7])+Integer.parseInt(line_buf[8])));
+			if (filetag == 1) {
+				if (line_buf[3].compareTo("Brand#34") == 0 && line_buf[6].compareTo("MED PACK") == 0) {
+					if (!(line_buf[3].compareTo("Brand#34") == 0 && line_buf[6].compareTo("MED PACK") == 0))
+						dispatch.set(5);
+					if (dispatch.isEmpty())
+						context.write(new IntWritable(Integer.parseInt(line_buf[0])), new Text(1+"||"+Integer.parseInt(line_buf[0])+ "|" ));
+					else
+						context.write(new IntWritable(Integer.parseInt(line_buf[0])), new Text(1+"|"+dispatch.toString()+"|"+Integer.parseInt(line_buf[0])+ "|" ));
+				}
 			}
-
-			if (filetag == 2){
-
-				context.write(new IntWritable(Integer.parseInt(line_buf[0])), new Text(2+"||"+line_buf[1]+Integer.parseInt(line_buf[0])));
+			if (filetag == 2) {
+				context.write(new IntWritable(Integer.parseInt(line_buf[1])), new Text(2+"||"+Integer.parseInt(line_buf[1])+ "|" +Double.parseDouble(line_buf[4])+ "|" +Double.parseDouble(line_buf[5])+ "|" ));
 			}
-
 		}
 
 	}
 
 	public static class Reduce extends Reducer<IntWritable, Text, NullWritable, Text> {
 
-		public void reduce(IntWritable key, Iterable<Text> v, Context context) throws IOExceptiuon, InterruptedException {
-
+		public void reduce(IntWritable key, Iterable<Text> v, Context context) throws IOException, InterruptedException {
 			Iterator values = v.iterator();
-			ArrayList[] tmp_output = new ArrayList[1];
-			for (int i = 0; i < 1; i++) {
-
+			ArrayList[] tmp_output = new ArrayList[3];
+			for (int i = 0; i < 3; i++) {
 				tmp_output[i] = new ArrayList();
 			}
 			String tmp = "";
-			ArrayList al_left_0 = new ArrayList();
-			ArrayList al_right_0 = new ArrayList();
-			while (values.hasNext()) {
+			Double[] result_0 = new Double[1];
+			ArrayList[] d_count_buf_0 = new ArrayList[1];
+			int al_line_0 = 0;
+			for (int i = 0; i < 1; i++) {
+				result_0[i] = 0.0;
+				d_count_buf_0[i] = new ArrayList();
+			}
 
+			ArrayList al_left_1 = new ArrayList();
+			ArrayList al_right_1 = new ArrayList();
+			ArrayList al_left_2 = new ArrayList();
+			ArrayList al_right_2 = new ArrayList();
+			while (values.hasNext()) {
 				String line = values.next().toString();
 				String dispatch = line.split("\\|")[1];
 				tmp = line.substring(2+dispatch.length()+1);
 				String[] line_buf = tmp.split("\\|");
-				if (line.charAt(0) == '1' && (dispatch.length() == 0 || dispatch.indexOf("1") == -1))
-					al_left_1.add(tmp);
+				if (line.charAt(0) == '2' && (dispatch.length() == 0 || dispatch.indexOf('0') == -1)) {
+					result_0[0] += Double.parseDouble(line_buf[1]);
+					al_line_0++;
+				}
 				if (line.charAt(0) == '2' && (dispatch.length() == 0 || dispatch.indexOf("1") == -1))
+					al_left_1.add(tmp);
+				if (line.charAt(0) == '1' && (dispatch.length() == 0 || dispatch.indexOf("1") == -1))
 					al_right_1.add(tmp);
 			}
 			String[] line_buf = tmp.split("\\|");
-			for (int i = 0; i < al_left_0.size(); i++) {
-				String[] left_buf_0 = ((String) al_left_0.get(i)).split("\\|");
-				for (int j = 0; j < al_right_0.size(); j++) {
-					String[] right_buf_0 = ((String) al_right_0.get(j)).split("\\|");
-					tmp_output[0].add(Integer.parseInt(left_buf_0[0])+left_buf_0[1]+Double.parseDouble(left_buf_0[2])+left_buf_0[3]+right_buf_0[0]+left_buf_0[4]+left_buf_0[5]+Double.parseDouble(left_buf_0[6])+Double.parseDouble(left_buf_0[7]));
+			result_0[0] = result_0[0] / al_line_0;
+			tmp_output[0].add(Integer.parseInt(line_buf[0]) + "|"+(0.2 * (result_0[0])) + "|");
+			for (int i = 0; i < al_left_1.size(); i++) {
+				String[] left_buf_1 = ((String) al_left_1.get(i)).split("\\|");
+				for (int j = 0; j < al_right_1.size(); j++) {
+					String[] right_buf_1 = ((String) al_right_1.get(j)).split("\\|");
+					tmp_output[1].add(Integer.parseInt(left_buf_1[0])+ "|" +Double.parseDouble(left_buf_1[1])+ "|" +Double.parseDouble(left_buf_1[2])+ "|" );
+				}
+			}
+			for (int i = 0; i < tmp_output[1].size(); i++) {
+				String[] left_buf_2 = ((String) tmp_output[1].get(i)).split("\\|");
+				for (int j = 0; j < tmp_output[0].size(); j++) {
+					String[] right_buf_2 = ((String) tmp_output[0].get(j)).split("\\|");
+					if (Double.parseDouble(left_buf_2[1]) < Double.parseDouble(right_buf_2[1])) {
+						tmp_output[2].add(1+ "|" +Double.parseDouble(left_buf_2[2])+ "|" );
+					}
 				}
 			}
 			NullWritable key_op = NullWritable.get();
-			for (int i = 0; i < tmp_output[0].size(); i++) {
-				String result = (String)tmp_output[0].get(i);
+			for (int i = 0; i < tmp_output[2].size(); i++) {
+				String result = (String)tmp_output[2].get(i);
 				context.write(key_op, new Text(result));
 			}
 		}
@@ -97,7 +120,6 @@ import org.apache.hadoop.mapreduce.lib.partition.*;
 	}
 
 	public int run(String[] args) throws Exception {
-
 		Configuration conf = new Configuration();
 		Job job = new Job(conf, "testquery2");
 		job.setJarByClass(testquery2.class);
@@ -105,17 +127,18 @@ import org.apache.hadoop.mapreduce.lib.partition.*;
 		job.setMapOutputValueClass(Text.class);
 		job.setOutputKeyClass(NullWritable.class);
 		job.setOutputValueClass(Text.class);
-setMapperClass(Map.class);
-		job.setReduceClass(Reduce.class);
-		FileOutputFormat.addInputPath(job, new Path(args[0]));
-		FileOutputFormat.addInputPath(job, new Path(args[1]));
-		FileOutput.setOutputPath(job, new Path(args[2]));
-		return (job.waitForCompletion) ? 0 : 1);
+		job.setMapperClass(Map.class);
+		job.setReducerClass(Reduce.class);
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileInputFormat.addInputPath(job, new Path(args[1]));
+		FileOutputFormat.setOutputPath(job, new Path(args[2]));
+		return (job.waitForCompletion(true) ? 0 : 1);
 	}
 
 	public static void main(String[] args) throws Exception {
-
 		int res = ToolRunner.run(new Configuration(), new testquery2(), args);
 		System.exit(res);
 	}
+
+}
 
