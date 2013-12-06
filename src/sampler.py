a039647ee5243ca9d579a98c5044aba0fe7c329b
yeyue <yeyue@ubuntu.(none)>
2013-12-06 21:30:25 +0800

    update 12.06

--- src/sampler.py
+++ src/sampler.py
@@ -18,14 +18,17 @@ import sys
 import config
 import os
 import random
-import pydoop.hdfs as hdfs
-import pydoop.hdfs.path as hpath
 import codegen
+import hadoopy
+import subprocess
+import job
 
 '''
 {
 "MAP_INPUT_BYTES":0,
 "MAP_INPUT_RECORDS":0,
+"MAP_OUTPUT_BYTES":0,
+"MAP_OUTPUT_RECORDS":0,
 "REDUCE_INPUT_BYTES":0,
 "REDUCE_INPUT_RECORDS":0,
 "REDUCE_OUTPUT_BYTES":0,
@@ -35,84 +38,114 @@ import codegen
 
 ratio = config.ratio
 tmp_path = config.hadoop_tmp_dir
+fs_tmp_path = config.fs_tmp_dir
+tmp_log_dir = config.tmp_log_dir
+data_dir = config.data_dir
 
 def doSample(jarfile, inputs, output, k):
-    tmp_inputs = []
     for item in inputs:
-        if hpath.isdir(item):
-            item = item[:-1]
-        name = hpath.basename(item)
-        tmp_dir = tmp_path + name + "/"
-        if not hpath.exists(tmp_dir):
-            hdfs.mkdir(tmp_dir)
-        tmp_inputs.append(tmp_dir)
-        for f in hdfs.ls(item):
-            if hpath.isfile(f):
-                ff = tmp_dir + hpath.basename(f)
+        if item[-1] == "/":
+            name = (item[:-1]).split('/')[-1]
+        else:
+            name = item.split('/')[-1]
+        print "item", item 
+        #tmp_dir = tmp_path + name + "/"
+        if hadoopy.exists(item):
+            continue
+        hadoopy.mkdir(item)
+        #tmp_inputs.append(tmp_dir)
+        real_input = data_dir + name + "/"
+        for f in hadoopy.ls(real_input):
+            if not hadoopy.isdir(f):
+                #ff = tmp_dir + f.split('/')[-1]
                 if k > 0:
-                    poolSample(f, ff, k)
+                    poolSample(f, item, k)
                 else:
-                    commonSample(f, ff, ratio)
-    if not hpath.exists(output):
-        hdfs.mkdir(output)
-    if hpath.isdir(output):
+                    commonSample(f, item, ratio)
+    '''if not hadoopy.exists(output):
+        hadoopy.mkdir(output)
+    if hadoopy.isdir(output):
         output = output[:-1]
-    name = hpath.basename(output)
-    tmp_output = tmp_path + name + "/"
+    if output[-1] == '/':
+        output = output[:-1]
+    name = output.split('/')[-1]
+    tmp_output = tmp_path + name + "/"'''
     #if not hpath.exists(tmp_output):
     #    hdfs.mkdir(tmp_output)
-    codegen.executeJar(jarfile, tmp_inputs, tmp_output)
+    codegen.executeJar(jarfile, inputs, output)
+    #jobid = job.getJobIDFromLog(tmp_log_dir)
+    job_para = job.getJobPara()
+    '''for item in tmp_inputs:
+        os.system("hadoop fs -rmr " + item)
+    os.system("hadoop fs -rmr " + tmp_output)'''
+    return job_para
 
 def commonSample(fip, fop, ratio):
-    fi = hdfs.open(fip, "r")
-    fo = hdfs.open(fop, "w")
+    tmpfip = fs_tmp_path + fip.split('/')[-1]
+    tmpfop = fs_tmp_path + "sample_" + (fop[:-1]).split('/')[-1]
+    #print "hadoop fs -get " + fip + " " + tmpfip
+    code = os.system("hadoop fs -get " + fip + " " + tmpfip)
+    print "code", code
+    #child = subprocess.Popen("hadoop fs -get " + fip + " " + tmpfip, stderr=subprocess.PIPE, shell=True)
+    #child.wait()
+    fi = open(tmpfip, "r")
+    fo = open(tmpfop, "w")
     count = 0
-    line = fi.readline()
-    while line != "":
+    for f in fi:
         count += 1
-        line = fi.readline()
     k = int(count * ratio)
     print "count, ratio, k", count, ratio, k
     line_list = random.sample(range(0, count), k)
     line_list.sort()
     #print "line_list", line_list
-    
+    fi.seek(0)
     i = 0
     count = 0
-    fi.seek(0)
-    line = fi.readline()
-    while line != "" and i < k:
+    results = []
+    for f in fi:
+        if i >= k:
+            break
         if count == line_list[i]:
-            fo.write(line)
+            results.append(f)
             i += 1
         count += 1
-        line = fi.readline()
-
+    fo.writelines(results)
     fi.close()
     fo.close()
+    print "hadoop fs -put " + tmpfop + " " + fop
+    code = os.system("hadoop fs -put " + tmpfop + " " + fop)
+    #child = subprocess.Popen("hadoop fs -put " + tmpfop + " " + fop, stderr=subprocess.PIPE, shell=True)
+    #child.wait()
+    #os.system("rm " + tmpfip)
+    #os.system("rm " + tmpfop)
 
 def poolSample(fip, fop, k):
-    fi = hdfs.open(fip, "r")
-    fo = hdfs.open(fop, "w")
-
+    tmpfip = fs_tmp_path + fip.split('/')[-1]
+    tmpfip = fs_tmp_path + fip.split('/')[-1]
+    tmpfop = fs_tmp_path + "sample_" + (fop[:-1]).split('/')[-1]
+    #print "hadoop fs -get " + fip + " " + tmpfip
+    code = os.system("hadoop fs -get " + fip + " " + tmpfip)
+    print "code", code
+    fi = open(tmpfip, "r")
+    fo = open(tmpfop, "w")
     results = []
     i = 0
-    line = fi.readline()
-    while line != "":
+    for f in fi:
         if i < k:
-            results.append(line)
+            results.append(f)
         else:
             r = random.randrange(0, i)
             if r < k:
-                results[r] = line
+                results[r] = f
         i += 1
-        line = fi.readline()
 
-    for line in results:
-        fo.write(line)
+    fo.writelines(results)
     
     fi.close()
     fo.close()
+    os.system("hadoop fs -put " + tmpfop + " " + fop)
+    os.system("rm " + tmpfip)
+    os.system("rm " + tmpfop)
 
 def _sample(_file, k):
     f = open(_file, "r")
